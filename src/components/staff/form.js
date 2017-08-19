dab00352808f83335ba0604409c216087754f469
atiqahyahya <nuratiqahyahya1994@gmail.com>
2017-08-19 22:25:00 +0800

    edit form and button

--- src/components/staff/form.js
+++ src/components/staff/form.js
@@ -4,7 +4,8 @@ import {
   FormGroup,
   FormControl,
   ControlLabel,
-  Form
+  Form,
+  Col
 
 } from 'react-bootstrap';
 
@@ -12,61 +13,56 @@ import './index.css';
 
 
 class Staff extends Component {
-  constructor(props) {
-      super(props);
-  }
 
   render() {
     return (
       <div>
-        <Form>
-          <FormGroup controlId="formInlineName">
-            <ControlLabel>Name</ControlLabel>
-            {' '}
-            <FormControl type="text" placeholder="Jane Doe" />
-          </FormGroup>
-          {' '}
-          <FormGroup controlId="formInlineEmail">
-            <ControlLabel>Email</ControlLabel>
-            {' '}
-            <FormControl type="email" placeholder="jane.doe@example.com" />
-          </FormGroup>
-          {' '}
+         <Col xs={10} md={3}>
+            <Form>
+              <FormGroup controlId="formInlineName">
+                <ControlLabel>Name</ControlLabel>
+                {' '}
+                <FormControl type="text" placeholder="Jane Doe" />
+              </FormGroup>
+                {' '}
 
-           <FormGroup controlId="formInlineMobileNo">
-            <ControlLabel>Mobile Number</ControlLabel>
-            {' '}
-            <FormControl type="mobileNo" placeholder="0176545240" />
-          </FormGroup>
-          {' '}
+              <FormGroup controlId="formInlineEmail">
+                <ControlLabel>Email</ControlLabel>
+                {' '}
+                <FormControl type="email" placeholder="jane.doe@example.com" />
+              </FormGroup>
+              {' '}
 
-           <FormGroup controlId="formInlineAddress">
-            <ControlLabel>Address</ControlLabel>
-            {' '}
-            <FormControl type="address" placeholder="N0 40, Jalan Tunggal" />
-          </FormGroup>
-          {' '}
+               <FormGroup controlId="formInlineMobileNo">
+                <ControlLabel>Mobile Number</ControlLabel>
+                {' '}
+                <FormControl type="mobileNo" placeholder="0176545240" />
+              </FormGroup>
+              {' '}
 
-           <FormGroup controlId="formInlineEcontact">
-            <ControlLabel>Emergency Contact</ControlLabel>
-            {' '}
-            <FormControl type="eContact" placeholder="0145879635" />
-          </FormGroup>
-          {' '}
+               <FormGroup controlId="formInlineAddress">
+                <ControlLabel>Address</ControlLabel>
+                {' '}
+                <FormControl type="address" placeholder="N0 40, Jalan Tunggal" />
+              </FormGroup>
+              {' '}
 
-          <center>
-            <Button type="submit"  bsStyle="primary">
-              Save
-            </Button>
+               <FormGroup controlId="formInlineEcontact">
+                <ControlLabel>Emergency Contact</ControlLabel>
+                {' '}
+                <FormControl type="eContact" placeholder="0145879635" />
+              </FormGroup>
+              {' '}
+
+           <center>
+            <p>
+              <Button bsStyle="primary">Save</Button>&nbsp;
+              <Button bsStyle="default">Cancel</Button>
+            </p>
           </center>
 
-          <center>
-            <Button type="submit">
-            Cancel
-            </Button>
-          </center>
-
-        </Form>
+          </Form>
+        </Col>
       </div>
       );
   }
--- src/components/staff/index.js
+++ src/components/staff/index.js
@@ -5,60 +5,63 @@ import {
   //FormControl,
   //ControlLabel,
   Table,
+  Col
 
 } from 'react-bootstrap';
 
 import './index.css';
 
-
 class Staff extends Component {
-	constructor(props) {
-	    super(props);
-	}
 
   render() {
     return (
-      <div>
-      	<h1>Staff</h1>
-			<Table striped bordered condensed hover>
-				    <thead>
-				      <tr>
-				        <th>#</th>
-				        <th>First Name</th>
-				        <th>Last Name</th>
-				        <th>Mobile Number</th>
-				        <th>Address</th>
-				        <th>Emergency Contact</th>
-				      </tr>
-				    </thead>
-				    <tbody>
-				      <tr>
-				        <td>1</td>
-				        <td>Mark</td>
-				        <td>Otto</td>
-				        <td>@mdo</td>
-				        <td>asasas</td>
-				        <td>017</td>
-				      </tr>
-				      <tr>
-				        <td>2</td>
-				        <td>Jacob</td>
-				        <td>Thornton</td>
-				        <td>@fat</td>
-				        <td>asasa</td>
-				        <td>012</td>				     
-				      </tr>
-				      <tr>
-				        <td>3</td>
-				        <td>say</td>
-				        <td>Larry the Bird</td>
-				        <td>@twitter</td>
-				        <td>swdg</td>
-				        <td>014</td>
-				      </tr>
-				    </tbody>
-			</Table>   	
-      </div>
+      	<div>
+      		<Col  xs={25} md={6}>
+      		
+		      	<center>
+		      		<h1>Staff</h1>
+		      	</center>
+
+					<Table striped bordered condensed hover>
+						    <thead>
+						      <tr>
+						        <th>#</th>
+						        <th>First Name</th>
+						        <th>Last Name</th>
+						        <th>Mobile Number</th>
+						        <th>Address</th>
+						        <th>Emergency Contact</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						      <tr>
+						        <td>1</td>
+						        <td>Mark</td>
+						        <td>Otto</td>
+						        <td>@mdo</td>
+						        <td>asasas</td>
+						        <td>017</td>
+						      </tr>
+						      <tr>
+						        <td>2</td>
+						        <td>Jacob</td>
+						        <td>Thornton</td>
+						        <td>@fat</td>
+						        <td>asasa</td>
+						        <td>012</td>				     
+						      </tr>
+						      <tr>
+						        <td>3</td>
+						        <td>say</td>
+						        <td>Larry the Bird</td>
+						        <td>@twitter</td>
+						        <td>swdg</td>
+						        <td>014</td>
+						      </tr>
+						    </tbody>
+					</Table>
+			</Col>
+     	</div>
     );
     
   }
